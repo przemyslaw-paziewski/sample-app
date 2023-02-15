d4ecbf1a23799b444d6bc56e91b2d0745a40e8ee
Przemek Paziewski <ppaazz56@gmail.com>
2023-02-15 19:40:14 +0100

    feat / mui, react-query add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"
+import { ReactQueryDevtools } from "react-query/devtools"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  )
 }
